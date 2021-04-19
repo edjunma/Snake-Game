1ea7192a3a1c3c56d7ed0ed19de1318e48e1d1f2
edjunma <edjunma@gmail.com>
2021-04-18 23:46:31 -0400

    4/18 update

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -11,11 +11,13 @@ const Board = () => {
   return (
     <div className="board">
       {board.map((row, rowIdx) => (
-        <div key={rowIdx} className="row">{
-          row.map((cell, cellIdx) => (
-            <div key={cellIdx} className="cell"></div>  
-          ))
-        }</div>
+        <div key={rowIdx} className="row">
+          {row.map((cell, cellIdx) => (
+            <div 
+            key={cellIdx} 
+            className={`cell $true ? 'snake-cell' : ''}`}></div>  
+          ))}
+          </div>
       ))}
     </div>
   )
