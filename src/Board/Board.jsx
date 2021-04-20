d3ee888ede211a28ef698913f25ae49bbdff1009
edjunma <edjunma@gmail.com>
2021-04-19 23:16:50 -0400

    add row loop to createBoard function, add direction components

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -65,6 +65,18 @@ const Board = () => {
 const createBoard = BOARD_SIZE => {
   let counter = 1;
   const board = [];
+  for (let row = 0; row < BOARD_SIZE; row++) {
+    const currentRow = [];
+    for (let col = 0; col < BOARD_SIZE; col++) {
+      currentRow.push(counter++);
+    }
+    board.push(currentRow);
+  }
+  return board;
+}
+
+const getDirectionFromKey = key => {
+  return '';
 }
 
 export default Board;
