fbaf543837f2477a5296687372f8640019be7d99
edjunma <edjunma@gmail.com>
2021-04-20 23:50:17 -0400

    add Direction components

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -76,6 +76,10 @@ const createBoard = BOARD_SIZE => {
 }
 
 const getDirectionFromKey = key => {
+  if (key === 'ArrowUp') return Direction.UP;
+  if (key === 'ArrowRight') return Direction.RIGHT;
+  if (key === 'ArrowDown') return Direction.DOWN;
+  if (key === 'ArrowLeft') return Direction.LEFT;
   return '';
 }
 
